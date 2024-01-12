376c2b3fd93f5ba9a11b50b5357ac54cfb4bc8ff
Bhavik Patel <patelbj2004@gmail.com>
2024-01-12 11:36:31 +0530

    added dark mode and router functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
+import React,{useState} from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import About from './components/About';
+
 
 function App() {
-  return (
-    <>
-      <Navbar title="Textutils" about="About textutils"/>
-      <div className="container">
 
-      <Form heading="Enter text below to analyze"/>
-      </div>
-    </>
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = ()=>{
+    if(mode==="light"){
+      setMode('dark')
+      document.body.style.background = '#2C3333';
+    }
+    else{
+      setMode('light')
+      document.body.style.background = 'white';
+    }
+  }
+  return (
+
+    <BrowserRouter>
+      <Routes>
+      
+        <Route path='/' element={<Navbar title="Textutils" about="About textutils" mode={mode}  toggleMode={toggleMode}/>} >
+
+            <Route index element={<div className="container">
+              <Form heading="Enter text below to analyze" mode={mode} />
+              </div>}/>
+            <Route path='/about' element={<About mode={mode} />}/>
+        </Route>
+        
+        
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -51,21 +51,21 @@ export default function Form(props) {
 
   return (
     <>
-        <div className='container my-2 border border-black'>
+        <div className={`container my-2 border border-${props.mode==='light'?'dark':'light'} bg-${props.mode==='light'?'light':'dark'} text-${props.mode==='light'?'dark':'light'}`}>
         
                 <h1>{props.heading}</h1>
                 <div class="mb-3">
-                <textarea class="form-control" value={text} id="textAreaId" onChange={handleOnChange} rows="8" placeholder='Enter your text here'></textarea>
+                <textarea className={`form-control bg-${props.mode==='light'?'light':'secondary'} text-${props.mode==='light'?'dark':'light'}`}  value={text} id="textAreaId" onChange={handleOnChange} rows="8" placeholder='Enter your text here'></textarea>
                 </div>
                 <div>
-                    <button className='mx-2 my-2 btn btn-primary'  type="button" onClick={handleUpClick}>Convert to Uppercase</button>
-                    <button className='mx-2 my-2 btn btn-primary'  type="button" onClick={handleLowClick} >Convert to Lowercase</button>
-                    <button className='mx-2 my-2 btn btn-primary'  type="button" onClick={handleCapClick} >Capitalize text</button>
-                    <button className='mx-2 my-2 btn btn-primary'  type="button" onClick={handleCopy} >Copy text</button>
-                    <button className='mx-2 my-2 btn btn-primary'  type="button" onClick={handleClear} >Clear text</button>
+                    <button className={`mx-2 my-2 btn btn-${props.mode==='light'?'primary':'secondary'} `}  type="button" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className={`mx-2 my-2 btn btn-${props.mode==='light'?'primary':'secondary'} `}  type="button" onClick={handleLowClick} >Convert to Lowercase</button>
+                    <button className={`mx-2 my-2 btn btn-${props.mode==='light'?'primary':'secondary'} `}  type="button" onClick={handleCapClick} >Capitalize text</button>
+                    <button className={`mx-2 my-2 btn btn-${props.mode==='light'?'primary':'secondary'} `}  type="button" onClick={handleCopy} >Copy text</button>
+                    <button className={`mx-2 my-2 btn btn-${props.mode==='light'?'primary':'secondary'} `}  type="button" onClick={handleClear} >Clear text</button>
                 </div>
         </div>
-        <div className="container border border-black">
+        <div className={`container border border-${props.mode==='light'?'dark':'light'} bg-${props.mode==='light'?'light':'dark'} text-${props.mode==='light'?'dark':'light'}`}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").filter(checkSpace).length} word and {text.length} characters</p>
                 <h3>Preview</h3>
